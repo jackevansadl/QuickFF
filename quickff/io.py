aaeabc57452fbcaee9e31c63e426894b67f9493d
Jack Evans <jack.evans.adl@gmail.com>
2022-07-21 09:35:46 +0930

    added orca ab initio support

--- quickff/io.py
+++ quickff/io.py
@@ -982,3 +982,130 @@ def dump_yaff(valence, fn):
         print('', file=f)
         print('', file=f)
     f.close()
+
+# hess and grad reader adapted from https://github.com/glibaniosr/ORCA-read-for-glowfreqs/blob/master/orcaread-gf.py
+from os import read
+
+def make_array(r,c):
+  array = [[0 for col in range(c)] for row in range(r)]
+  return array
+
+def read_orca_hess_grad(hess_file, grad_file):
+
+    with open(hess_file, 'r') as inp:
+        for line in inp:
+            if not "$hessian" in line: # Flag for the Hessian group
+                continue
+            else:
+                ## Get the Hessian size and other parameters
+                # ORCA prints the Hessian in text with all lines from 5 columns each time.
+                # Therefore, it will be printed n5_sets = hess_size//5 sets with hess_size number of lines.
+                # In the last set will be printed rest = hess_size%5 columns with hess_size lines.
+                # OBS: From version 4.1.2, ORCA started to print the Hessian in sets of 6 columns
+                for data in inp:
+                    ##--- Important Parameters
+                    hess_size = int(data.strip())
+                    n_atoms = int(hess_size/3)
+                    n5_sets = hess_size//5 # Number of 5 columns sets.
+                    rest = hess_size%5
+
+                    if rest == 0:
+                        n5_sets = n5_sets-1 # If the size is divisible by 5, then the sets would be larger.
+                        rest = 5
+                    ## Create the HESSIAN array
+                    hess_data = make_array(hess_size,hess_size+1)
+                    ## Write the number of the output line in the first element of the hess_data matrix
+                    for n_line in range(hess_size):
+                        hess_data[n_line][0] = n_line+1
+                    next(inp) # Jump the second line.
+                    break
+                #for data in inp: # Jump the second line (improve this)
+                #    break
+                count = 0
+                while count <= n5_sets:
+                    count2 = 0
+                    ## First get the data from all sets of 5 columns except the last one if rest
+                    # is not zero.
+                    if count != n5_sets:
+                        for data in inp:
+                            columns = [ int(num) for num in range(count*5,(count)*5+5) ]
+                            data = data.split()
+                            n_line = int(data[0])
+                            if  count2 >= hess_size:
+                                break
+                            for i in range(5):
+                                j = columns[i] + 1
+                                hess_data[n_line][j] = float(data[i+1])
+                            count2 += 1
+                    ## This is the condition for the last sets of column that will not be zero
+                    # if the size of the HESS matrix is not divisible by 5.
+                    else:
+                        for data in inp:
+                            columns = [ int(num) for num in range(count*5,(count)*5+rest) ]
+                            if data.strip() == '':
+                                break
+                            data = data.split()
+                            n_line = int(data[0])
+                            if  count2 >= hess_size:
+                                break
+                            for i in range(rest):
+                                j = columns[i] + 1
+                                hess_data[n_line][j] = float(data[i+1])
+                            count2 += 1
+                    count += 1
+
+    hess_data = np.array(hess_data)
+    hess_data = np.delete(hess_data, 0, 1)
+
+
+    elements = []
+    masses = []
+    xyz_data = make_array(n_atoms,3)
+    with open(hess_file, 'r') as inp:
+        for line in inp:
+            if not "$atoms" in line: # Flag for the Hessian group
+                continue
+            else:
+                next(inp) # Jump the first line with the number of atoms.
+                for idx,data in enumerate(inp):
+                    if data.strip() == '':
+                        break
+                    data = data.strip().split()
+                    elements.append(data[0])
+                    masses.append(float(data[1]))
+                    xyz_data[idx] = [float(data[2]), float(data[3]), float(data[4]) ]
+
+
+    grad_data = make_array(n_atoms,3)
+    ## Get the gradient information from the .engrad file
+    with open(grad_file, 'r') as inp:
+        for line in inp:
+            if not "gradient in Eh/bohr" in line: # Flag for gradient group, and possibility to write on file.
+                continue
+            else:
+                next(inp) # Jump the first line with a comment symbol.
+                xyz_temp = []
+                for idx,data in enumerate(inp):
+                    if data.strip() == '#':
+                        break
+                    data = data.strip()
+                    xyz_temp.append(float(data))
+                    if (idx+1)%3 == 0:
+                        nline = (idx+1)//3 - 1
+                        grad_data[nline] = [float(xyz_temp[0]), float(xyz_temp[1]), float(xyz_temp[2]) ]
+                        xyz_temp = []
+
+    with open(grad_file) as f:
+        lines = f.readlines()
+        energy=lines[7]
+
+    numbers = np.zeros(len(elements), int)
+    for index, element in enumerate(elements):
+        number = periodic[element].number
+        numbers[index] = number
+
+    # orca is molecular data
+    rvecs = None
+    pbc = [0,0,0]
+
+    return(numbers, np.array(xyz_data), float(energy), np.array(grad_data), hess_data, np.array(masses), rvecs, pbc)
